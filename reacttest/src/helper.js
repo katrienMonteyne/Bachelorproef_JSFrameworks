c0e07ec7268492950d1013cb1a25a49654191551
Katrien Monteyne <>
2018-03-02 17:04:33 +0100

    form opzetten

---
+++ reacttest/src/helper.js
@@ -0,0 +1,19 @@
+var axios = require('axios');
+
+function getUser(userID){
+  return axios.get('http://localhost:3004/users/' + userID);
+}
+
+
+var helpers = {
+  getGithubInfo: function(userID){
+    return axios.all([getUser(userID)])
+      .then(function(arr){
+        return {
+          user: arr[0].data
+        }
+      })
+  }
+}
+
+module.exports = helpers;
--- reacttest/src/index.js
+++ reacttest/src/index.js
@@ -1,141 +1,162 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
 
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
+class Personalia extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null)
-      }],
-      xIsNext: true,
-      stepNumber: 0
+      user: null,
+      edit: true
     }
+
   }
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3004/users/1")
+      .then(response => {
+        this.setState({
+          user: response.data
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
+
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares}
-                 onClick={(i) => this.handleClick(i)}/>
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
+      <div>
+        {this.state.edit ? <Editpersonalia user={this.state.user} /> : <Showpersonalia user={this.state.user} />}
+      </div>
+    )
+
+  }
+
+};
+
+class Showpersonalia extends React.Component {
+
+  render() {
+    let firstname, lastname, address, age, email, sex;
+
+    if (this.props.user) {
+      const user = this.props.user;
+
+      if (user.firstname != null && user.firstname != '') {
+        firstname = <p>{user.firstname}</p>
+      } else { firstname = <p className="red">Voornaam nog niet ingevuld</p> }
+
+      if (user.lastname != null && user.lastname != '') {
+        lastname = <p>{user.lastname}</p>
+      } else { lastname = <p className="red">Achternaam nog niet ingevuld</p> }
+
+      if (user.address != null && user.address != '') {
+        address = <p>{user.address}</p>
+      } else { address = <p className="red">Adres is nog niet ingevuld</p> }
+
+      if (user.age != null && user.age != '') {
+        age = <p>{user.age}</p>
+      } else { age = <p className="red">Leeftijd is nog niet ingevuld</p> }
+
+      if (user.email != null && user.email != '') {
+        email = <p>{user.email}</p>
+      } else { email = <p className="red">Emailadres is nog niet ingevuld</p> }
+
+      if (user.sex != null && user.sex != '') {
+        sex = <p>{user.sex}</p>
+      } else { sex = <p className="red">Geslacht is nog niet ingevuld</p> }
+    }
+
+    return (
+      <div>
+        {firstname}
+        {lastname}
+        {address}
+        {age}
+        {email}
+        {sex}
       </div>
     );
   }
+
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+class Editpersonalia extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {firstname: null}
     }
+    this.handleChange = this.handleChange.bind(this);
   }
-  return null;
-}
 
-// ========================================
+  handleChange(event){
+    console.log('Boe');
+    this.props.user.firstname = event.target.value;
+  }
+
+  render() {
+
+    let user = null;
+    let firstname, lastname, age, sex, address, email;
+
+    if(this.props.user){
+      user = this.props.user;
+      this.setState({
+        user: this.props.user
+      });
+      // elementen mogen niet null zijn voor in het input field => lege string declareren
+      if(user.firstname  == null){ user.firstname =''}
+      if(user.lastname  == null){ user.lastname =''}
+      if(user.age  == null){ user.age =''}
+      if(user.sex  == null){ user.sex =''}
+      if(user.address  == null){ user.address =''}
+      if(user.email  == null){ user.email =''}
+      firstname = <input type="text" name="name" value={user.firstname} onChange={this.handleChange}/>;
+      lastname = <input type="text" name="name" value={user.lastname}/>
+      age = <input type="text" name="name" value={user.age}/>
+      sex = <input type="text" name="name" value={user.sex}/>
+      address = <input type="text" name="name" value={user.address}/>
+      email = <input type="text" name="name" value={user.email}/>
+    }
+
+
+    return (
+      <form>
+        <label>
+          Voornaam: {firstname}
+        </label> <br/>
+        <label>
+          Achternaam: {lastname}
+        </label><br/>
+        <label>
+          Leeftijd: {age}
+        </label><br/>
+        <label>
+          Geslacht: {sex}
+        </label><br/>
+        <label>
+          Address: {address}
+        </label> <br/>
+        <label>
+          Email: {email}
+        </label>
+
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+
+
+}
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+  <Personalia />,
+  document.getElementById('personalia')
 );
